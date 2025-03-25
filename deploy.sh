e71b0f2ac084481a3dd6075cf781a2041be356c5
yumin-jung <jungym887@naver.com>
2025-03-25 23:40:08 +0900

    add AWS deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "> Blog Deploy"
+sudo su
+cd /home/ec2-user/blog
+chmod +x ./deploy.sh
+npm -v
+npm install next@latest
+pm2 delete blog
+pm2 start "npx next start" --name blog
