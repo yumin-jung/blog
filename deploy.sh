f7a843bcbc7c0fa7619fa36a2fd4c37c70dd0f7c
yumin-jung <jungym887@naver.com>
2025-03-26 00:26:58 +0900

    fix some ci scripts

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
-#!/usr/bin/env bash
-echo "> Blog Deploy"
-sudo su
-cd /home/ec2-user/blog
-chmod +x ./deploy.sh
-pnpm -v
-pnpm install next@latest
-pm2 delete blog
-pm2 start "npx next start" --name blog
+REPOSITORY=/home/ubuntu/deploy
+
+cd $REPOSITORY
+
+pnpm deploy
