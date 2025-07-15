d719f33d789b81741f2170ed74590762a3b5b7be
yumin-jung <jungym887@gmail.com>
2025-07-16 01:39:56 +0900

    remove: remove about page

--- app/header.tsx
+++ app/header.tsx
@@ -9,13 +9,6 @@ export function Header() {
 
       <nav className="font-mono text-xs grow justify-end items-center flex gap-1 md:gap-3">
         <ThemeToggle />
-
-        <Link
-          href="/about"
-          className="inline-flex hover:bg-gray-200 dark:hover:bg-[#313131] active:bg-gray-300 dark:active:bg-[#242424] rounded-sm p-2 transition-[background-color]"
-        >
-          About
-        </Link>
         <a
           href="https://github.com/yumin-jung"
           target="_blank"
